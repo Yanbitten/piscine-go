3335c94f73738f04b4709a53b8c6764c7ec28fee
Yanbitten <polorotova.yana@gmail.com>
2019-10-16 18:23:38 +0600

    fgdf

--- isnegative.go
+++ isnegative.go
@@ -7,7 +7,7 @@ func IsNegative(nb int) {
 	trueval := 'T'
 	falsval := 'F'
 	if nb >= 0 {
-		z01.PrintRune(falseval)
+		z01.PrintRune(falsval)
 		z01.PrintRune(10)
 	} else {
 		z01.PrintRune(trueval)
