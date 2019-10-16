a7fc2d351d79c7b87f371b35f30fedd09ce5c283
Yanbitten <polorotova.yana@gmail.com>
2019-10-16 18:16:17 +0600

    isn

---
+++ isnegative.go
@@ -0,0 +1,17 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func IsNegative(nb int) {
+
+	trueval := 'T'
+	falsval := 'F'
+	if nb >=0  {
+		z01.PrintRune(falseval)
+		z01.PrintRune(10)
+	} else {
+		z01.PrintRune(trueval)
+		z01.PrintRune(10)
+	}
+
+}
