c512b1e53564241c27fec88b8c1893a95c486a97
Yanbitten <polorotova.yana@gmail.com>
2019-10-17 22:05:51 +0600

    prstr

--- printstr.go
+++ printstr.go
@@ -2,10 +2,10 @@ package piscine
 
 import "github.com/01-edu/z01"
 
- func PrintStr (str string) { 
- 	for _, Yanbitten := range str {
+func PrintStr(str string) {
+	for _, Yanbitten := range str {
 
- 		z01.PrintRune
- 	}
+		z01.PrintRune
+	}
 
 }
