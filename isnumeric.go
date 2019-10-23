660e1ed3b36a395f36cb67e66db14a0c87df6107
Yanbitten <polorotova.yana@gmail.com>
2019-10-23 21:21:02 +0600

    sds57

--- isnumeric.go
+++ isnumeric.go
@@ -1,16 +1,24 @@
 package piscine
 
 func IsNumeric(str string) bool {
-	if str == "" {
-		return false
-	}
+
 	s := []rune(str)
-	for _, letter := range s {
-		if letter >= '0' && letter <= '9' {
-			continue
-		} else {
+	length := 0
+	for range str {
+		length++
+	}
+	for i := 0; i < length; i++ {
+		if !isNumA(s[i]) {
 			return false
 		}
 	}
 	return true
+
+}
+
+func isNumA(r rune) bool {
+	if r >= '0' && r <= '9' {
+		return true
+	}
+	return false
 }
